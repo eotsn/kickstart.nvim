d5f3fff24cbcaaa88231d585c9f0c2c040a7e46b
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2023-10-14 21:43:58 +0200

    feat: enable the gopls language server

--- init.lua
+++ init.lua
@@ -457,7 +457,7 @@ require('which-key').register({
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
